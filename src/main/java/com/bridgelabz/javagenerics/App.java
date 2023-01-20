55beffb1bf6dc695fe8961f6217aa9c75089af62
AryaBhuwan-98 <aryabhuwan873@gmail.com>
2023-01-20 13:31:34 -0800

    Add:Welcome to the JAVA GENERICS

---
+++ src/main/java/com/bridgelabz/javagenerics/App.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.javagenerics;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to the java Generics" );
+    }
+}
