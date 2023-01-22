43986d7d97298aa335241f9c2fd1091f16f2759d
AryaBhuwan-98 <aryabhuwan873@gmail.com>
2023-01-21 23:20:01 -0800

    Add:Adding program of extend the max method to take more then three parameters

--- src/main/java/com/bridgelabz/javagenerics/MaxString.java
+++ src/main/java/com/bridgelabz/javagenerics/MaxString.java
@@ -20,10 +20,10 @@ public class MaxString {
     public static void main(String[] args) {
         MaxString m = new MaxString();
         System.out.println("max Number for 1st test case=" + m.compareTO("Apple", "Banana", "Peach"));//Test case 1 :given max number for 1st position.
-       
+       //a=-1,b=-15,c=-14
         System.out.println("max Number for 2nd test case=" + m.compareTO("Peach", "Banana", "Apple"));//Test case 2:given max number for 2nd position
-        
+        //a=14,b=15,c=1
         System.out.println("max Number for 3rd test case=" + m.compareTO("Banana", "Peach", "Apple"));//Test case 3:given max number for the 3rd position.
-        
+        //a=14,b=1,c=15
     }
 }
